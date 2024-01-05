7fdd3ba03ca51906e5df27206ab94b300eec9184
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2024-01-05 17:42:13 +0530

    Add todos using form

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -13,32 +13,55 @@ function AddTodo({ onNewItem }) {
         setDueDate(event.target.value)
     }
 
-    const handleAddButtonClick = () => {
+    const handleAddButtonClick = (event) => {
+        event.preventDefault()              // use only while using form
         onNewItem(todoName, dueDate)
         setTodoName('')
         setDueDate('')
     }
 
+    // return (
+    //     <div className="container item-container">
+    //         <div className="row my-row">
+    //             <div className="col-6">
+    //                 <input type="text" placeholder="Enter todo here" 
+    //                  value={todoName}
+    //                  onChange={handleNameChange}/>
+    //             </div>
+    //             <div className="col-4">
+    //                 <input type="date" value={dueDate} onChange={handleDueDateChange} />
+    //             </div>
+    //             <div className="col-2">
+    //                 <button type="button" className="btn btn-success my-button"
+    //                     onClick={handleAddButtonClick}>
+    //                     Add
+    //                 </button>
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
+
+    // Add todos using form 
     return (
         <div className="container item-container">
-            <div className="row my-row">
+            <form className="row my-row" onSubmit={handleAddButtonClick}>
                 <div className="col-6">
-                    <input type="text" placeholder="Enter todo here" 
-                     value={todoName}
-                     onChange={handleNameChange}/>
+                    <input type="text" placeholder="Enter todo here"
+                        value={todoName}
+                        onChange={handleNameChange} />
                 </div>
                 <div className="col-4">
                     <input type="date" value={dueDate} onChange={handleDueDateChange} />
                 </div>
                 <div className="col-2">
-                    <button type="button" className="btn btn-success my-button"
-                        onClick={handleAddButtonClick}>
+                    <button className="btn btn-success my-button">
                         Add
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     );
+
 }
 
 export default AddTodo;
